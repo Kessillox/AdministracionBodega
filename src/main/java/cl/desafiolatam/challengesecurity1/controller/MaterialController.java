dc88cf76650a1927ec48131a117f14d7fa564db1
LucianoCesc <cisternas.luciano@gmail.com>
2022-11-28 22:36:19 -0300

    mas comentarios

--- src/main/java/cl/desafiolatam/challengesecurity1/controller/MaterialController.java
+++ src/main/java/cl/desafiolatam/challengesecurity1/controller/MaterialController.java
@@ -14,16 +14,17 @@ import java.util.List;
 @Controller
 public class MaterialController {
 
-    @GetMapping("/materiales")
+    @GetMapping("/materiales")//metodo get que abarca la ruta materiales
     public ModelAndView home(HttpSession session) {
-        ModelAndView modelAndView = new ModelAndView("materiales");
-        modelAndView.addObject("material", new Material());
-        modelAndView.addObject("materiales", session.getAttribute("materiales"));
-        return modelAndView;
+        ModelAndView modelAndView = new ModelAndView("materiales");//Crea un objeto tipo VistaModelo de nombre materiales
+        modelAndView.addObject("material", new Material());//luego, le agrega como atributo un objeto de tipo Material () de nombre "material"
+        modelAndView.addObject("materiales", session.getAttribute("materiales"));//luego, le agrega como atributo todos los materiales que esten en la sesión http q ingresa por parámetro y los nombra "materiales"
+        return modelAndView;// retorna todo el objeto
     }
 
-    @PostMapping("/materiales/agregar")
-    public RedirectView home(HttpSession session, @ModelAttribute Material material) {
+    @PostMapping("/materiales/agregar")//Le asigna la solicitud http post al metodo
+    public RedirectView home(HttpSession session, @ModelAttribute Material material) {//Los valores recibidos desde el formulario son almacenados directamente en la sesión. Se agrega a una lista de valores en la que se ingresa desde las vistas con el nombre valores.
+
         List<Material> materials = new ArrayList<>();
         if (session.getAttribute("materiales") != null)
             materials.addAll((List<Material>) session.getAttribute("materiales"));
