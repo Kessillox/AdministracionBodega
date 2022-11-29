543876fca92ac244e87edadda207934030badf73
Silvia2015 <silvitacabellocatapablo@gmail.com>
2022-11-29 20:55:57 +1200

    fin de los comentarios

--- src/test/java/cl/desafiolatam/challengesecurity1/ChallengeSecurity1ApplicationTests.java
+++ src/test/java/cl/desafiolatam/challengesecurity1/ChallengeSecurity1ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChallengeSecurity1ApplicationTests {
 
-	@Test
+	@Test // testeo de springboot
 	void contextLoads() {
 	}
 
