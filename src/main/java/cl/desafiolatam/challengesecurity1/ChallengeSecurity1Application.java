c5310c2c613461b9c86038aa32fb7c73a4abe8aa
Lisbeth Calcina <jliscayo@gmail.com>
2022-11-28 21:23:54 -0300

    comentarios

--- src/main/java/cl/desafiolatam/challengesecurity1/ChallengeSecurity1Application.java
+++ src/main/java/cl/desafiolatam/challengesecurity1/ChallengeSecurity1Application.java
@@ -3,11 +3,11 @@ package cl.desafiolatam.challengesecurity1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ChallengeSecurity1Application {
+@SpringBootApplication   // anotacion que indica que ejecuta la aplicacion del spring boot
+public class ChallengeSecurity1Application {  // Inicio de la Clase  
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengeSecurity1Application.class, args);
+	public static void main(String[] args) { // metodo main 
+		SpringApplication.run(ChallengeSecurity1Application.class, args); // Llamada a la aplicacion de tipo srping. 
 	}
 
 }
