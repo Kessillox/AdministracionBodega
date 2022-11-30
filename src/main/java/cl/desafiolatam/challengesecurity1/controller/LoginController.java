efa1afa47eef004e5183773dcc351aa9951f1174
Edgamer63 <edgamer63@gmail.com>
2022-11-29 22:24:42 -0300

    Solucion de Errores

--- src/main/java/cl/desafiolatam/challengesecurity1/controller/LoginController.java
+++ src/main/java/cl/desafiolatam/challengesecurity1/controller/LoginController.java
@@ -2,6 +2,7 @@ package cl.desafiolatam.challengesecurity1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +11,7 @@ public class LoginController {//se encarga de mostrar la página de inicio de se
     @GetMapping("/login") //nos permite simplificar el manejo de los diferentes métodos de Spring MVC y los @RequestMappings
     public ModelAndView login(){//Representa un modelo y una vista devueltos por un controlador, para ser resueltos por un DispatcherServlet.
 
-        return new ModelAndView("");//Retorna
+        return new ModelAndView("login");//Retorna
     }
 
 }
--- src/main/java/cl/desafiolatam/challengesecurity1/controller/MaterialController.java
+++ src/main/java/cl/desafiolatam/challengesecurity1/controller/MaterialController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class MaterialController {
 
-    @GetMapping("/materiales")//metodo get que abarca la ruta materiales
+	@GetMapping("/materiales")//metodo get que abarca la ruta materiales
     public ModelAndView home(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView("materiales");//Crea un objeto tipo VistaModelo de nombre materiales
         modelAndView.addObject("material", new Material());//luego, le agrega como atributo un objeto de tipo Material () de nombre "material"
@@ -22,15 +22,17 @@ public class MaterialController {
         return modelAndView;// retorna todo el objeto
     }
 
-    @PostMapping("/materiales/agregar")//Le asigna la solicitud http post al metodo
-    public RedirectView home(HttpSession , @ModelAttribute Material material) {//Los valores recibidos desde el formulario son almacenados directamente en la sesión. Se agrega a una lista de valores en la que se ingresa desde las vistas con el nombre valores.
+	@PostMapping("/materiales/agregar")//Le asigna la solicitud http post al metodo
+    public RedirectView home(HttpSession session , @ModelAttribute Material material) {//Los valores recibidos desde el formulario son almacenados directamente en la sesión. Se agrega a una lista de valores en la que se ingresa desde las vistas con el nombre valores.
 
-        List<Material> materials =  ArrayList<>();									//Se crea una lista de "Material"es
-        if (session.getAttribute("materiales") != null)									//Si la sesion http tiene la lista de "materiales", copia sus elementos a la lista de este metodo (materials)
-            materials.addAll((List<Material>) session.getAttribute("materiales"));
+		List<Material> materials = new ArrayList<>();									//Se crea una lista de "Material"es 
+        if (session.getAttribute("materiales") != null) {									//Si la sesion http tiene la lista de "materiales", copia sus elementos a la lista de este metodo (materials)
+            materials.addAll(materials);
+            session.getAttribute("materiales");
+        }
         materials.add(material);														//Luego le agrega a la sesion, el valor recibido desde el formulario y redirige a la ruta "/materiales"
         session.setAttribute("materiales", materials);
-
+     
         return new RedirectView("/materiales");
     }
 
